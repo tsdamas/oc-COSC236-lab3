0565572c213a20876e0e8ebb5173a895109334e6
Munashe Sydney <munashesydney@github.com>
2024-10-17 00:41:10 -0700

    Changed price to priceInDollars in MenuItem class

--- src/restaurant/MenuItem.java
+++ src/restaurant/MenuItem.java
@@ -2,18 +2,20 @@ package restaurant;
 // MenuItem class
 public class MenuItem {
     private String name;
-    private double price;
+    //changed price to priceInDollars
+    private double priceInDollars;
 
     public MenuItem(String name, double price) {
         this.name = name;
-        this.price = price;
+        this.priceInDollars = price;
     }
 
     public String getName() {
         return name;
     }
 
-    public double getPrice() {
-        return price;
+    public double getPriceInDollars() {
+    	// changed return of price to priceInDollars
+        return priceInDollars;
     }
 }
