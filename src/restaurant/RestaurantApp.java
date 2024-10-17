dc7986b900269580b4133162cdd1f5c9d595a8ff
Thais Damasceno <85142451+tsdamas@users.noreply.github.com>
2024-10-16 20:45:39 -0700

    update restaurant and order
    add msg every item added, change to one dollar to each menu item and change name of customer

--- src/restaurant/RestaurantApp.java
+++ src/restaurant/RestaurantApp.java
@@ -3,12 +3,12 @@ package restaurant;
 public class RestaurantApp {
     public static void main(String[] args) {
         // Creating customer
-        Customer customer = new Customer("Alice");
+        Customer customer = new Customer("Bob");
         
         // Creating menu items
-        MenuItem pizza = new MenuItem("Pizza", 8.99);
-        MenuItem burger = new MenuItem("Burger", 5.49);
-        MenuItem soda = new MenuItem("Soda", 1.99);
+        MenuItem pizza = new MenuItem("Pizza", 9.99);
+        MenuItem burger = new MenuItem("Burger", 6.49);
+        MenuItem soda = new MenuItem("Soda", 2.99);
         
         // Customer adds items to their order
         customer.addMenuItemToOrder(pizza);  // Collaborating with MenuItem and Order
