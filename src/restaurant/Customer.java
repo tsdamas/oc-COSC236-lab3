724738f93c4055c22ad2a93a448aec8496e53f89
sullywilliams <sullivan.williams@myokanagan.bc.ca>
2024-10-17 13:36:49 -0700

    Customer Changes
    changed method name for
    addMenuItemToOrder(MenuItem item)
    to
    addToOrder(MenuItem item)

--- src/restaurant/Customer.java
+++ src/restaurant/Customer.java
@@ -9,7 +9,7 @@ public class Customer {
         this.order = new Order();  // Collaborating with Order class
     }
 
-    public void addMenuItemToOrder(MenuItem item) {
+    public void addToOrder(MenuItem item) {
         order.addItem(item);  // Collaborating with Order to add items
     }
 
